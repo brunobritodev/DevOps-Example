a4e3ff31f88088d1415c39fa0dde534701a9bfd5
Bruno Brito <bhdebrito@gmail.com>
2019-07-19 13:57:41 -0300

    removendo database

--- DevOps-Example/Startup.cs
+++ DevOps-Example/Startup.cs
@@ -1,9 +1,7 @@
-﻿using DevOps.Data;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,8 +29,6 @@ namespace DevOps
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddDbContext<DevOpsContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("DevOpsContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
